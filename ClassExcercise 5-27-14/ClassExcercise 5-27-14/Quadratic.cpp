26a7822762340ad0a813c757e02ecc191eea7523
lsmith17 <lsmith17@my.whitworth.edu>
2014-05-28 19:41:41 -0700

    Fixed Code

--- ClassExcercise 5-27-14/ClassExcercise 5-27-14/Quadratic.cpp
+++ ClassExcercise 5-27-14/ClassExcercise 5-27-14/Quadratic.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 #include <cmath>
-#include <Quadratic.h>
 class QuadraticEquation
 {
+private:
+	int a;
+	int b;
+	int c;
 public:
-		
-			float QuadraticEquation::getDescriminant(){
-				discriminant = ((b*b) - (4 * a*c));
-				if (a == 0)
-				{
-					cout << "Can't divide by zero" << endl;
-				}
-				
-				if (discriminant < 0)
-				{
-					cout << "Sorry, can't produce an imaginary number" << endl;
-				}
-				else(discriminant > 0);
-			
-				float QuadraticEquation::getRoot1(){
-				root1 = ((-1 * b) + (sqrt(discriminant)) / (2 * a));
-			}
-			float QuadraticEquation::getRoot2(){
-				root2 = ((-1 * b) - (sqrt(discriminant)) / (2 * a));
-			}
+	QuadraticEquation(double a, double b, double c){
+	}
+	float getA(){
+		return a;
+	}
+	float getB(){
+		return b;
+	}
+	float getC(){
+		return c;
+	}
+	float getDiscriminant() {
+		return (b*b) - (4 * a*c);
+	}
+	float getRoot1() {
+		if (getDiscriminant() < 0)
+			return 0;
+		else{
+		return (-b + sqrt(getDiscriminant())) / (2 * a);
+	}
+	}
+	float getRoot2() {
+		if (getDiscriminant() < 0)
+			return 0;
 
+		else{
+			return (-b - sqrt(getDiscriminant())) / (2 * a);
+		}
+	}
+};
 
+int main(){
+	
+	double a, b, c, discriminant;
+	cout << "Enter the three coefficients: a, b, c " << endl;
+	cin << a << b << c;
+
+	QuadraticEquation Q(a, b, c);
+	discriminant = Q.getDiscriminant;
+
+	if (discriminant < 0)
+		cout << "Can't compute. Imaginary number." << endl;
+	else if (discriminant == 0)
+		cout << "The root is: " << Q.getRoot1() << endl;
+	else {
+		cout << "The first root is: " << Q.getRoot1() << " The second root is: " << Q.getRoot2() << endl;
+
+		return 0;
 	}
 
-};
+
+}
